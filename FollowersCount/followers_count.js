b0316de5bfafc02d62ccc860d085850e417de74d
Mue <kritchanon.sev@gmail.com>
2026-02-14 14:36:31 +0700

    Add followers count

---
+++ FollowersCount/followers_count.js
@@ -0,0 +1,26 @@
+let count = 0;
+
+function increaseCount() {
+    count++;
+    displayCount();
+    congratulateMilestone();
+}
+
+function displayCount() {
+    document.getElementById('countDisplay').innerText = count;
+}
+
+function congratulateMilestone() {
+    if (count === 10) {
+        alert("Congratulations! You've reached a milestone of 10 followers!");
+    } else if (count === 50) {
+        alert("Amazing! You've reached a milestone of 50 followers!");
+    } else if (count === 100) {
+        alert("Incredible! You've reached a milestone of 100 followers!");
+    }
+}
+
+function resetCount() {
+    count = 0;
+    displayCount();
+}
