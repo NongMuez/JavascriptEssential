44efe0f74d32e9dda6a05cc2f14c59e17bb2dc34
Mue <kritchanon.sev@gmail.com>
2026-02-19 14:09:32 +0700

    fixed array

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -6,10 +6,12 @@ const breakfastMenuItem =  breakfastMenu
     .map((item, index) => `<p> Item ${index + 1} : ${item} </p>`).join('');
     document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItem;
 
-const mainCourseMenuItem =  mainCourseMenu
-    .map((item, index) => `<p> Item ${index + 1} : ${item} </p>`).join('');
-    document.getElementById('maincourseMenuItems').innerHTML = mainCourseMenuItem;
+let mainCourseItem = '';
+    mainCourseMenu.forEach((item, index) => {
+    mainCourseItem += `<p>Item ${index + 1}: ${item}</p>`;});
+    document.getElementById('maincourseMenuItems').innerHTML = mainCourseItem;
     
-const dessertMenuItem =  dessertMenu
-    .map((item, index) => `<p> Item ${index + 1} : ${item} </p>`).join('');
-    document.getElementById('dessertMenuItems').innerHTML = dessertMenuItem;    
+let dessertItem = '';
+for (let i = 0; i < dessertMenu.length; i++) {
+    dessertItem += `<p>Item ${i + 1}: ${dessertMenu[i]}</p>`;}
+document.getElementById('dessertMenuItems').innerHTML = dessertItem;
